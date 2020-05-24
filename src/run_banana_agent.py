61f4d05b92219acfebedd6dfcef910169369c438
Daniel Sotingco <daniel.sotingco@gmail.com>
2020-05-24 16:48:01 -0400

    First cut at running DQN on banana-collecting environment.

---
+++ src/run_banana_agent.py
@@ -0,0 +1,17 @@
+# load the weights from file
+agent.qnetwork_local.load_state_dict(torch.load('checkpoint.pth'))
+
+for i in range(3):
+    state = env.reset()
+    img = plt.imshow(env.render(mode='rgb_array'))
+    for j in range(200):
+        action = agent.act(state)
+        img.set_data(env.render(mode='rgb_array')) 
+        plt.axis('off')
+        display.display(plt.gcf())
+        display.clear_output(wait=True)
+        state, reward, done, _ = env.step(action)
+        if done:
+            break 
+            
+env.close()
---
+++ src/train_banana_agent.py
@@ -0,0 +1,76 @@
+"""train_banana_agent.py
+Train the banana-collecting agent.
+"""
+
+from unityagents import UnityEnvironment
+import numpy as np
+import torch
+from collections import deque
+from dqn_agent import Agent
+import matplotlib.pyplot as plt
+
+def dqn(agent, n_episodes=5, eps_start=1.0, eps_end=0.01, eps_decay=0.995):
+    """Deep Q-Learning.
+    
+    Params
+    ======
+        n_episodes (int): maximum number of training episodes
+        eps_start (float): starting value of epsilon, for epsilon-greedy action selection
+        eps_end (float): minimum value of epsilon
+        eps_decay (float): multiplicative factor (per episode) for decreasing epsilon
+    """
+    scores = []                        # list containing scores from each episode
+    scores_window = deque(maxlen=100)  # last 100 scores
+    eps = eps_start                    # initialize epsilon
+    for i_episode in range(0, n_episodes):
+        brain_info = env.reset(train_mode=True)[brain_name]
+        state = brain_info.vector_observations[0]
+        score = 0
+        while True:
+            action = agent.act(state, eps)
+            env_info = env.step(action)[brain_name]
+            next_state = env_info.vector_observations[0]
+            reward = env_info.rewards[0]
+            done = env_info.local_done[0]
+            score += reward
+            state = next_state
+            if done:
+                break 
+        scores_window.append(score)       # save most recent score
+        scores.append(score)              # save most recent score
+        eps = max(eps_end, eps_decay*eps) # decrease epsilon
+        print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)), end="")
+        if i_episode % 100 == 0:
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+        if np.mean(scores_window)>=200.0:
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_window)))
+            torch.save(agent.qnetwork_local.state_dict(), 'checkpoint.pth')
+            break
+    return scores
+
+env = UnityEnvironment(file_name="Banana.exe")
+
+# get the default brain
+brain_name = env.brain_names[0]
+brain = env.brains[brain_name]
+
+# reset the environment
+env_info = env.reset(train_mode=True)[brain_name]
+
+# create an Agent
+banana_action_size = brain.vector_action_space_size
+state = env_info.vector_observations[0]
+banana_state_size = len(state)
+agent = Agent(state_size=banana_state_size, action_size=banana_action_size, seed=0)
+
+# run DQN
+scores = dqn(agent)
+env.close()
+
+# plot the scores
+fig = plt.figure()
+ax = fig.add_subplot(111)
+plt.plot(np.arange(len(scores)), scores)
+plt.ylabel('Score')
+plt.xlabel('Episode #')
+plt.show()
